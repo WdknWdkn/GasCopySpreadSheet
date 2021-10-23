d581c5245b151925fffcca5b774b337bf152d76e
和田健太郎 <49826943+WdknWdkn@users.noreply.github.com>
2021-10-23 10:25:34 +0900

    Update code.js

--- code.js
+++ code.js
@@ -9,6 +9,7 @@ function makeTodaySheet() {
   copySheetArray.forEach(function(sheeName){
     AutoSheetCopy(sheeName);
   });
+  postSlack();
 }
 
 // シート"XXXXmaster"を複製する関数
@@ -28,4 +29,32 @@ function getDate(){
   // 現在の日時から、日付取得（月日で）
   var out = Utilities.formatDate(new Date(), "JST", "MMdd");
   return out; 
+}
+
+function postSlack() {
+
+    
+    const postUrl  = '';// slackのincoming webhook用
+    const username = '週報bot';  // 通知時に表示されるユーザー名
+    const icon     = ':hatching_chick:';  // 通知時に表示されるアイコン
+
+    const subject = '【今週の週報記載お願いします！】';
+    const ssURL = ''; // 本文にSSURLを記載
+    const body    = '' + subject + '\n' + ssURL + '\n';
+
+    const jsonData =
+              {
+                  'username'  : username,
+                  'icon_emoji': icon,
+                  'text'      : body
+              };
+
+    const options =
+              {
+                  'method'     : 'post',
+                  'contentType': 'application/json',
+                  'payload'    : JSON.stringify(jsonData)
+              };
+
+    UrlFetchApp.fetch(postUrl, options);
 }
